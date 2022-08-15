eb1ffe29e10dd436839d5ee8c84c54aefdc4f207
George Arutyunyan <arutunyan.george@gmail.com>
2022-08-15 13:02:47 +0300

    creating table updated

--- Homework_2/Insert.sql
+++ Homework_2/Insert.sql
@@ -1,39 +1,39 @@
-INSERT INTO singer 
+INSERT INTO singer (name_singer)
 VALUES 
-	(1, 'Rammstein'),
-	(2, 'System of a down'),
-	(3, 'Max Richter'),
-	(4, 'Ramin Djawardi'),
-	(5, 'Adele'),
-	(6, 'Miyagi'),
-	(7, 'Lera Lynn'),
-	(8, 'Armin van Buuren')
+	('Rammstein'),
+	('System of a down'),
+	('Max Richter'),
+	('Ramin Djawardi'),
+	('Adele'),
+	('Miyagi'),
+	('Lera Lynn'),
+	('Armin van Buuren')
 ;
 
-INSERT INTO gendre
+INSERT INTO genre (name_genre)
 VALUES
-	(1, 'Hip-hop'),
-	(2, 'Classic'),
-	(3, 'Pop'),
-	(4, 'Rock'),
-	(5, 'Trance')
+	('Hip-hop'),
+	('Classic'),
+	('Pop'),
+	('Rock'),
+	('Trance')
 ;
 
-INSERT INTO album
+INSERT INTO album (album_name, album_year)
 VALUES 
-	(1, 'Touch the sky', '2020-11-11'),
-	(2, 'Mutter', '2004-11-02'),
-	(3, 'Deutschland', '2019-03-31'),
-	(4, 'Chop Suey!', '2001-02-11'),
-	(5, 'Toxity', '2001-09-09'),
-	(6, '25', '2015-01-08'),
-	(7, '30', '2021-03-19'),
-	(8, 'Free again', '2022-03-19'),
-	(9, 'Dream 3', '2020-12-02')
+	('Touch the sky', '2020-11-11'),
+	('Mutter', '2004-11-02'),
+	('Deutschland', '2019-03-31'),
+	('Chop Suey!', '2001-02-11'),
+	('Toxity', '2001-09-09'),
+	('25', '2015-01-08'),
+	('30', '2021-03-19'),
+	('Free again', '2022-03-19'),
+	('Dream 3', '2020-12-02')
 ;
 
 INSERT INTO track
-	(track_name, duration, album_id)
+	(track_name, track_length , album_id)
 VALUES 
 	('Mutter', 231, 2),
 	('Sonne', 121, 2),
@@ -49,10 +49,11 @@ VALUES
 	('Lonely day', 312, 4),
 	('Aerials', 412, 5),
 	('Easy on me', 322, 7),
-	('Millions years ago', 412, 7)
+	('Millions years ago', 412, 7),
+	('Ohne dich', 310, 3)
 ;
 
-INSERT INTO singers_in_albums 
+INSERT INTO singer_in_album (singer_id, album_id)
 VALUES
 	(1, 2),
 	(1, 3),
@@ -65,7 +66,7 @@ VALUES
 	(8, 9)
 ;
 
-INSERT INTO gendre_singer 
+INSERT INTO genre_singer (genre_id, singer_id)
 VALUES 
 	(1, 6),
 	(2, 3),
@@ -78,16 +79,29 @@ VALUES
 ;
 
 INSERT INTO compilation 
-(name, release_date, album_id, track_id)
+(compilation_name, compilation_year)
 VALUES
-	('Favorite', '2022-02-11', 2, 1),
-	('Favorite', '2022-02-11', 2, 2),
-	('Favorite', '2022-02-11', 4, 3),
-	('Favorite', '2022-02-11', 4, 4),
-	('Favorite', '2022-02-11', 4, 12),
-	('pop_music', '2014-02-11', 6, 5),
-	('pop_music', '2014-02-11', 7, 7),
-	('pop_music', '2014-02-11', 8, 8),
-	('pop_music', '2014-02-11', 8, 9)
+	('Favorite', '2022-02-11'),
+	('Favorite', '2022-02-11'),
+	('Favorite', '2022-02-11'),
+	('Favorite', '2022-02-11'),
+	('Favorite', '2022-02-11'),
+	('pop_music', '2014-02-11'),
+	('pop_music', '2014-02-11'),
+	('pop_music', '2014-02-11'),
+	('pop_music', '2014-02-11')
 ;
-	
+
+INSERT INTO compilation_singer 
+(compilation_id, track_id)
+VALUES 
+	(1, 1),
+	(1, 2),
+	(1, 4),
+	(1, 5),
+	(1, 6),
+	(2, 3),
+	(2, 9),
+	(2, 10),
+	(2, 11)
+;
